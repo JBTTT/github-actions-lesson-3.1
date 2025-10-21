2e27af7a9b1faf523b960c9561e4b85c9e938386
jibin <perseverancejbt@hotmail.com>
2025-10-22 00:17:31 +0800

    Added with workflow despatch destroy with S3...

--- main.tf
+++ main.tf
@@ -2,18 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "bucket1" {
-  bucket = "jibin-s3.1-bucket999"  #Use a globally unique name
+resource "aws_s3_bucket" "jibin-bucket" {
+  bucket = "jibin-s3.1-bucket989"  #Use a globally unique name
+  acl = "private"
 }
 
-resource "aws_s3_bucket_public_access_block" "block_public_access" {
-  bucket = aws_s3_bucket.example_bucket.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
 
 terraform {
   backend "s3" {
