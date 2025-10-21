9e06a4a82a26dd7778aa53708344ffd1f425a7d1
jibin <perseverancejbt@hotmail.com>
2025-10-22 00:01:00 +0800

    Added with workflow despatch destroy with S3.

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_s3_bucket" "bucket1" {
+  bucket = "jibin-s3.1-bucket999"  #Use a globally unique name
+  force_destroy = true
+}
+
 terraform {
   backend "s3" {
     bucket = "jibin-s3.1-bucket999"
