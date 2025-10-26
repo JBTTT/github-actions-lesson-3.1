986c68e02a2c4e2e6396251fba9103be13adbc55
jibin <perseverancejbt@hotmail.com>
2025-10-26 23:41:46 +0800

    Assignment3.1 Pre-run 1

--- main.tf
+++ main.tf
@@ -2,15 +2,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "jibin-bucket" {
-  bucket = "jibin-s3.1-bucket989"  #Use a globally unique name
-  acl = "public"
+terraform {
+  backend "s3" {
+    bucket = "jibin-s3.1-bucket989"
+    key    = "jibin-s3.1-bucket989.tfstate"
+    region = "us-east-1"
+  }
 }
-
-# terraform {
-#  backend "s3" {
-#    bucket = "jibin-s3.1-bucket999"
-#    key    = "jibin-s3.1-bucket999.tfstate"
-#    region = "us-east-1"
-#  }
-#}
